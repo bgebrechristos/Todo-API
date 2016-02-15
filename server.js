87960af8cac2b9582277eb3ac518d08f824c6ab2
Brook Gebrechristos <brookzer@gmail.com>
2016-02-14 16:23:38 -0800

    final static todos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var todoNextId = 1;
 
 app.use(bodyParser.json())
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -18,6 +18,12 @@ app.get('/todos', function(req, res) {
         filteredTodos = _.filter(filteredTodos, {'completed' : true})
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos= _.filter(filteredTodos, {'completed' : false})
+    }
+    
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(item) {
+            return item.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
     }
     
     res.json(filteredTodos);
